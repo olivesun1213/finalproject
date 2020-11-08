44a100e77f46d027b665791687db0ee633c9f1bd
neha-nayeem <61065609+neha-nayeem@users.noreply.github.com>
2020-11-08 14:36:34 -0500

    Updated flask app with model prediction and routes

--- app.py
+++ app.py
@@ -19,41 +19,103 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/historicaldata")
+def historical():
+    return render_template("historical.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/models")
+def getmodels():
+    return render_template("models.html")
 
 @app.route('/predict', methods=["GET", "POST"])
 def predict():
+
     if request.method == "POST": 
-        # getting input with name = hour in HTML form 
-        #hourURL = request.form["hour"]
-        #hour = requests.get(hourURL)
-        # getting input with name = road_class in HTML form  
-        #road_class = request.form.get("road_class") 
-        data = request.form
-        for key in data:
-            print ('form key '+key+" "+data[key])
+        # getting user input from HTML form
+        hour = request.form["hour"]
+        road_class = request.form["road_class"]
+        traffic_ctl = request.form["traffic_control"]
+        visibility = request.form["visibility"]
+        light = request.form["light"]
+        condition = request.form["condition"]
 
-        #print(data) 
-        #print(hourURL)
-        #print(road_class)
-        #return "Data: "+hour + road_class 
+        # encoding string input to binary value arrays for the model prediction
+        hour_array = onehot_encode("hour", hour)
+        road_class_array = onehot_encode("road_class", road_class)
+        traffic_ctl_array = onehot_encode("traffic_control", traffic_ctl)
+        visibility_array = onehot_encode("visibility", visibility)
+        light_array = onehot_encode("light", light)
+        condition_array = onehot_encode("condition", condition)
 
-    data = [[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
-    prediction = model.predict(data)
-    #prediction = np.array2string(model.predict(data))
-    print(prediction)
-    #return jsonify(prediction)
+        # joining the arrays into one with 66 features
+        data_array = [hour_array + road_class_array + traffic_ctl_array + visibility_array + light_array + condition_array]
 
-# # function to one hot encode user input for model prediction
-# def onehot_encode(feature, value):
-#     hour_array = []
-#     if 
-#     switch (feature):
-#         case "hour":
+        # print(hour_array)
+        # print(road_class_array)
+        # print(traffic_ctl_array)
+        # print(visibility_array)
+        # print(light_array)        
+        # print(condition_array)   
+        # print(data_array)
+        # print(len(data_array))
 
+    # predicting fatal or non-fatal with our model
+    prediction = np.array2string(model.predict(data_array))
+
+    return render_template("prediction.html", pred = prediction)
+
+# function to one hot encode user input for model prediction
+def onehot_encode(feature, value):
+
+    if feature == "hour":
+        array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        options = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
+        for i in range(len(options)):
+            if options[i] == value:
+                array[i] = 1
+    elif feature == "road_class":
+        array = [0,0,0,0,0,0,0,0]
+        options = ["Collector", "Expressway", "Laneway", "Local", "Major Arterial", "Major Arterial Ramp", "Minor Arterial", "Pending"]
+        for i in range(len(options)):
+            if options[i] == value:
+                array[i] = 1
+    elif feature == "traffic_control":
+        array = [0,0,0,0,0,0,0,0,0,0,0]
+        options = ["No Control", "Pedestrian Crossover", "Police Control", "PXO - No Ped", "School Guard", "Stop Sign", "Streetcar (Stop for)", "Traffic Controller", "Traffic Gate", "Traffic Signal", "Yield Sign"]
+        for i in range(len(options)):
+            if options[i] == value:
+                index = i
+                array[index] = 1  
+    elif feature == "visibility":
+        array = [0,0,0,0,0,0,0]
+        options = ["Clear", "Drifting Snow", "Fog, Mist, Smoke, Dust", "Freezing Rain", "Rain", "Snow", "Strong wind"]
+        for i in range(len(options)):
+            if options[i] == value:
+                index = i
+                array[i] = 1   
+    elif feature == "light":
+        array = [0,0,0,0,0,0,0,0]
+        options = ["Dark", "Dark, artificial", "Dawn", "Dawn, artificial", "Daylight", "Daylight, artificial", "Dusk", "Dusk, artificial"]
+        for i in range(len(options)):
+            if options[i] == value:
+                index = i
+                array[i] = 1
+    elif feature == "condition":
+        array = [0,0,0,0,0,0,0,0]
+        options = ["Dry", "Ice", "Loose Sand or Gravel", "Loose Snow", "Packed Snow", "Slush", "Spilled liquid", "Wet"]
+        for i in range(len(options)):
+            if options[i] == value:
+                index = i
+                array[i] = 1
+
+    return array
 
 
 if __name__ == '__main__':
-    #modelfile = 'models/final_prediction.pickle'
     modelfile = 'models/DecisionTree_final_model.pickle'
     model = p.load(open(modelfile, 'rb'))
     app.run(debug=True, host='0.0.0.0')
